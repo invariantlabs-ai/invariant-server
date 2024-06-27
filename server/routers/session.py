5ca84372e3527049ef9459aab34d08d23da6cd4d
adrgs <albastroiudragos@gmail.com>
2024-06-27 16:41:21 +0200

    Fix session route

--- server/routers/session.py
+++ server/routers/session.py
@@ -4,10 +4,10 @@ from .. import crud, schemas, database
 
 router = APIRouter()
 
-@router.get("/session/new", response_model=schemas.SessionBase)
+@router.get("/new", response_model=schemas.SessionBase)
 def create_session(db: Session = Depends(database.get_db)):
     return crud.create_session(db)
 
-@router.get("/session", response_model=schemas.SessionBase)
+@router.get("/", response_model=schemas.SessionBase)
 def read_session(session_id: str, db: Session = Depends(database.get_db)):
     return crud.get_session(db, session_id)
