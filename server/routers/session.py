d2a64435435696a11cb36a3779c3423b1aff059d
adrgs <albastroiudragos@gmail.com>
2024-06-27 15:17:10 +0200

    Implement session API

---
+++ server/routers/session.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from .. import crud, schemas, database
+
+router = APIRouter()
+
+@router.get("/session/new", response_model=schemas.SessionBase)
+def create_session(db: Session = Depends(database.get_db)):
+    return crud.create_session(db)
+
+@router.get("/session", response_model=schemas.SessionBase)
+def read_session(session_id: str, db: Session = Depends(database.get_db)):
+    return crud.get_session(db, session_id)
