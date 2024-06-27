504172e7271538340172b815113b213fc70dd540
adrgs <albastroiudragos@gmail.com>
2024-06-27 14:39:09 +0200

    Add models

---
+++ server/models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Text, UniqueConstraint
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+import uuid
+
+Base = declarative_base()
+
+class Session(Base):
+    __tablename__ = 'sessions'
+    id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4()))
+
+class Policy(Base):
+    __tablename__ = 'policies'
+    policy_id = Column(Integer, primary_key=True, index=True)
+    session_id = Column(String, ForeignKey('sessions.id'), primary_key=True)
+    rule = Column(Text, nullable=False)
+    UniqueConstraint('policy_id', 'session_id', name='unique_policy_per_session')
+
+class Monitor(Base):
+    __tablename__ = 'monitors'
+    monitor_id = Column(Integer, primary_key=True, index=True)
+    session_id = Column(String, ForeignKey('sessions.id'), primary_key=True)
+    policy_id = Column(Integer, ForeignKey('policies.policy_id'))
+    trace = Column(Text, nullable=True)
+    UniqueConstraint('monitor_id', 'session_id', name='unique_monitor_per_session')
+
+    policy = relationship("Policy")
+    session = relationship("Session")
