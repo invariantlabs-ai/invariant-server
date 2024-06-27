0c9bf7a558b31cc54e84d82521ebb56eb1a0c9de
adrgs <albastroiudragos@gmail.com>
2024-06-27 18:15:04 +0200

    Add support for templates

--- server/routers/policy.py
+++ server/routers/policy.py
@@ -2,12 +2,23 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
 from .. import crud, schemas, database, ipc
+import os
 
 router = APIRouter()
 
 @router.get("/", response_model=List[schemas.Policy])
 def read_policies(session_id: str, db: Session = Depends(database.get_db)):
     return crud.get_policies(db, session_id)
+
+@router.get("/templates")
+def get_policy_templates():
+    def build_json_structure(path):
+        if os.path.isdir(path):
+            return {item: build_json_structure(os.path.join(path, item)) for item in os.listdir(path)}
+        else:
+            with open(path, 'r', encoding='utf-8') as file:
+                return file.read()
+    return build_json_structure("server/templates")
 
 @router.post("/new", response_model=schemas.Policy)
 def create_policy(session_id:str, policy: schemas.PolicyCreate, db: Session = Depends(database.get_db)):
