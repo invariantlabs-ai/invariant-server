6c213e2b1e3504f5aba408196b33cf33f40e7a39
adrgs <albastroiudragos@gmail.com>
2024-08-16 19:35:12 +0200

    Remove other caches since it doesn't work on remote

--- server/ipc/invariant-ipc.py
+++ server/ipc/invariant-ipc.py
@@ -39,16 +39,16 @@ def worker(client_socket):
         client_socket.close()
 
 if __name__ == "__main__":
-    # cache invariant functions
-    prompt_injection([Message(role="user", content="test")])
-    pii([Message(role="user", content="test")])
-    moderated([Message(role="user", content="test")])
-    semgrep([Message(role="user", content="print(1)")], lang="python")
-
     mp.set_start_method("fork")
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(('0.0.0.0', 9999))
     server_socket.listen(1024)
+
+    pii([Message(role="user", content="test")])
+    # cache invariant functions
+    #prompt_injection([Message(role="user", content="test")])
+    #moderated([Message(role="user", content="test")])
+    #semgrep([Message(role="user", content="print(1)")], lang="python")
 
     while True:
         client_sock, _ = server_socket.accept()
